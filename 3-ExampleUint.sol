13a7dddd35b9255dba04314b5fa0e8adc6ce35f1
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-02 22:27:34 +0100

    Update 3-ExampleUint.sol

--- 3-ExampleUint.sol
+++ 3-ExampleUint.sol
@@ -1,3 +1,27 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.7;
+
+contract ExampleUint{
+    uint256 public myUint; //0 - (2^256)-1
+
+    uint8 public myUint8 =250;
+
+    int public myInt = -10; // -2^128 to +2^128-1
+
+    function setMyUint(uint _myUint) public{
+        myUint = _myUint;
+    }
+
+    function decrementUint() public{
+        myUint--;
+    }
+
+    function increementUint8() public{
+        myUint8++;
+    }
+
+    function incrementint() public{
+        myInt++;
+    }
+}
