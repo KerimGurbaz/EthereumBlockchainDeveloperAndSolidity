34155b49bb9cc2ee1cf13a26c663380aaeb5c2ee
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-04 23:27:54 +0100

    Update 5-ExampleStrings.sol

--- 5-ExampleStrings.sol
+++ 5-ExampleStrings.sol
@@ -1,3 +1,17 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity 0.8.15;
+
+
+contract ExampleStrings{
+    string public myString = "Hello World";
+
+    function SetMyString(string memory _myString ) public{
+         myString = _myString;  
+    }
+
+    function compareTwoStrings(string memory _myString) public view returns(bool){
+        return keccak256(abi.encodePacked(myString)) ==keccak256(abi.encodePacked(_myString));
+    }
+ 
+}
