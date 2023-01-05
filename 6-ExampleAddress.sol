9086646f835ac8e3ed1003741c531267ae2814b0
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-05 13:56:15 +0100

    Update 6-ExampleAddress.sol

--- 6-ExampleAddress.sol
+++ 6-ExampleAddress.sol
@@ -1,3 +1,16 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity 0.8.15;
+
+contract ExampleAddress{
+    address public someAddress;
+
+    function setSomeAddress(address _someAddress) public {
+
+        someAddress = _someAddress;
+    }
+
+    function getAddressBalances() public view returns(uint){
+        return someAddress.balance;
+    }
+}
