6a256c2dd0b70c22cd68fd3795871dadf0af93f6
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-07 14:02:44 +0100

    pure and view func.

--- 8-ExampleViewPure.sol
+++ 8-ExampleViewPure.sol
@@ -2,3 +2,22 @@
 
 pragma solidity 0.8.15;
 
+contract ExampleViewPure{
+
+    uint public myStorageVariable;
+
+
+    function getMyStorageVariable() public view returns(uint){
+        return myStorageVariable;
+    }
+
+    function getAddition(uint a, uint b) public pure returns(uint){
+        return a+b;
+    }
+
+    function setMyStorageVarable(uint _newVar) public returns(uint){
+        myStorageVariable= _newVar;
+        return _newVar;
+
+    }
+}
