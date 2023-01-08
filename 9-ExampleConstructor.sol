1733fd845449ff6d5be8b558bd93cdf5685cf354
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-08 21:55:34 +0100

    Update 9-ExampleConstructor.sol

--- 9-ExampleConstructor.sol
+++ 9-ExampleConstructor.sol
@@ -1,1 +1,17 @@
 
+contract ExampleConstructor{
+
+ address public myAddress;
+
+ constructor(address _someAddress){
+     myAddress = _someAddress;
+ }
+
+ function setMyAddress(address _myAddress) public{
+     myAddress = _myAddress;
+ }
+
+ function setMyAddressToMsgSender() public {
+     myAddress = msg.sender;
+ }
+}
