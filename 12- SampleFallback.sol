7c4e6e423c78cd658f2c2713854f22a6318ae9a6
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-29 00:25:43 +0100

    Update 12- SampleFallback.sol

--- 12- SampleFallback.sol
+++ 12- SampleFallback.sol
@@ -1,1 +1,15 @@
 
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.15;
+
+
+contract SampleFallback{
+
+    uint public lastValueSent;
+    string public lastFunctionCalled;
+    
+    receive() external payable{
+        lastValueSent = msg.value;
+        lastFunctionCalled = 'receive';
+    }
+}
