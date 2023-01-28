40594db81c4685c395973ed636aef29107198aef
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-29 00:41:01 +0100

    Update 12- SampleFallback.sol

--- 12- SampleFallback.sol
+++ 12- SampleFallback.sol
@@ -6,10 +6,22 @@ pragma solidity 0.8.15;
 contract SampleFallback{
 
     uint public lastValueSent;
+    
     string public lastFunctionCalled;
+    
+    uint public myUnit;
+
+    function setMyUint(uint _myNewUint) public {
+        myUnit = _myNewUint;
+    }
     
     receive() external payable{
         lastValueSent = msg.value;
         lastFunctionCalled = 'receive';
+    }
+    
+    fallback() external payable{
+        lastValueSent = msg.value;
+        lastFunctionCalled = 'fallback';
     }
 }
