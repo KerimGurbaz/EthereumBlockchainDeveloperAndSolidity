f02679e6435b4009e04d27c444541434f8dcb7ad
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-31 00:11:20 +0100

    Update 16- ExampleStruct.sol

--- 16- ExampleStruct.sol
+++ 16- ExampleStruct.sol
@@ -1,3 +1,24 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity 0.8.15;
+
+
+contract Wallet {
+    address sender ;
+    uint valueSent;
+
+    function payContract() public payable{
+        sender = msg.sender;
+        valueSent = msg.value;
+    }
+}
+
+contract PaymentReceived{
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount){
+        from = _from;
+        amount = _amount;
+    }
+}
