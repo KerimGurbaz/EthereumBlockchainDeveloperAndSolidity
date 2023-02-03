4d3ab1f28e234407692e5fb0fb75e07c2b4b7b5d
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-03 23:08:13 +0100

    Update 19-ExampleTryCatch.sol

--- 19-ExampleTryCatch.sol
+++ 19-ExampleTryCatch.sol
@@ -8,3 +8,15 @@ contract WillThrow{
         require(false, 'Error message');
     }
 }
+
+contract ErrorHandling{
+    event ErrorLogging(string reason);
+    function catchTheError() public{
+        WillThrow will = new WillThrow();
+        try will.aFunction(){
+            // add code here if it works
+        } catch Error(string memory reason){
+            emit ErrorLogging(reason);
+        }
+    }
+}
