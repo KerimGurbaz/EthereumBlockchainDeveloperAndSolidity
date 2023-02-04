07f0000ce36310a6248ac9e5294e8a63de1ad9de
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-04 19:57:14 +0100

    Update 20- ExampleSendTransfer.sol

--- 20- ExampleSendTransfer.sol
+++ 20- ExampleSendTransfer.sol
@@ -1,3 +1,11 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity 0.8.15;
+
+    function withdrawTransfer(address payable _to) public{
+        _to.transfer(10);
+    }
+
+    function withdrawSend(address payable _to) public{
+        _to.send(10);
+    }
