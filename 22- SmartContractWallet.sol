7777b94b75a0182240e0acea2477185950f2a374
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-05 23:51:07 +0100

    Update 22- SmartContractWallet.sol

--- 22- SmartContractWallet.sol
+++ 22- SmartContractWallet.sol
@@ -4,4 +4,19 @@ pragma solidity 0.8.15;
 
 contract SmartContractWallet {
 
+    address payable owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory){
+        require(msg.sender == owner, "You are not the owner, aborting");
+
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require(success, "Aborting, call was not successful");
+        return returnData;
+    }
+
+    receive() external payable{}
 }
