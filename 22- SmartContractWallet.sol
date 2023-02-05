590f58275b8ab64ce7f4c05bd17f2cd942abb83f
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-06 00:32:56 +0100

    Update 22- SmartContractWallet.sol

--- 22- SmartContractWallet.sol
+++ 22- SmartContractWallet.sol
@@ -9,6 +9,19 @@ contract SmartContractWallet {
     constructor(){
         owner = payable(msg.sender);
     }
+    
+    
+    function setAllowance(address _for, uint _amount) public{
+        require(msg.sender == owner, "you are not the owner, aborting");
+        allowance[msg.sender] = _amount;
+
+
+        if(_amount > 0) {
+            isAllowedToSend[_for] = true;
+        } else{
+            isAllowedToSend[_for] = false;
+        }
+    }
 
     function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory){
         require(msg.sender == owner, "You are not the owner, aborting");
