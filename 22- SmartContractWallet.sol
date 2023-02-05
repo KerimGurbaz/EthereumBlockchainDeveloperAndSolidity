09144fd878dff03b80d93fb2e0e18c29e5928abd
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-06 00:48:01 +0100

    Update 22- SmartContractWallet.sol

--- 22- SmartContractWallet.sol
+++ 22- SmartContractWallet.sol
@@ -2,15 +2,30 @@
 
 pragma solidity 0.8.15; 
 
-contract SmartContractWallet {
+contract SmartContractWallet{
 
     address payable owner;
+
+    mapping(address => uint) public allowance;
+    mapping(address => bool) public isAllowedToSend;
+
+    mapping(address => bool) public guardians;
+    address payable nextOwner;
+    uint guardiansResetCount;
+    uint public constant confirmationsFromGuardiansForReset = 3;
 
     constructor(){
         owner = payable(msg.sender);
     }
-    
-    
+
+    function setGuardian(address _guardian, bool _isGuardian) public{
+        require(msg.sender == owner, "you are not the owner, aborting");
+        guardians[_guardian] = _isGuardian;
+    }
+
+  
+  
+}
     function setAllowance(address _for, uint _amount) public{
         require(msg.sender == owner, "you are not the owner, aborting");
         allowance[msg.sender] = _amount;
@@ -24,7 +39,14 @@ contract SmartContractWallet {
     }
 
     function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory){
-        require(msg.sender == owner, "You are not the owner, aborting");
+        // require(msg.sender == owner, "You are not the owner, aborting");
+
+        if(msg.sender != owner){
+            require(allowance[msg.sender] >= _amount, "You are trying to send more than you are allowed to aborting");
+            require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this smart contract, aborting");
+
+            allowance[msg.sender] -= _amount;
+        }
 
         (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
         require(success, "Aborting, call was not successful");
@@ -32,4 +54,5 @@ contract SmartContractWallet {
     }
 
     receive() external payable{}
+
 }
