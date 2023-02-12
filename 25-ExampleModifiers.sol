5e17c4876622e453b9e42f3a8425dc880349a265
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-12 21:34:19 +0100

    Update 25-ExampleModifiers.sol

--- 25-ExampleModifiers.sol
+++ 25-ExampleModifiers.sol
@@ -20,4 +20,21 @@ contract InheritanceModifierExample{
         require(msg.sender == owner, "You are not allowed");
         tokenBalance[owner]++;
     }
+    
+     function burnToken() public{
+        require(msg.sender ==owner, "You are not allowed") ;
+        tokenBalance[owner]--;
+    }
+
+    function purchaseToken() public payable{
+        require((tokenBalance[owner] * tokenPrice) / msg.value >0, "not enough tokens");
+        tokenBalance[owner] -= msg.value / tokenPrice;
+        tokenBalance[msg.sender] += msg.value /tokenPrice;
+    }
+
+    function sendToken(address _to, uint _amount) public{
+        require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
+        tokenBalance[msg.sender] -= _amount;
+        tokenBalance[_to] += _amount;
+    }
 }
