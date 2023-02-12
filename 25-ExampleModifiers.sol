9c4347dd9545ad4357804ffe131659de8b18fd85
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-12 20:58:27 +0100

    Create 25-ExampleModifiers.sol

---
+++ 25-ExampleModifiers.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.16; 
+
+
+contract InheritanceModifierExample{
+    mapping(address => uint) public tokenBalance;
+
+    address owner;
+
+    uint tokenPrice =1 ether;
+}
