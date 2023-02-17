f9f391aea402dc911ed6b057fd99973fde735e82
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-17 13:35:33 +0100

    Update 28-SampleVerification.sol

--- 28-SampleVerification.sol
+++ 28-SampleVerification.sol
@@ -8,4 +8,13 @@ contract MyContract{
     constructor(){
         balance[msg.sender] = 100;
     }
+    
+     function transfer(address to, uint amount) public{
+        balance[msg.sender] -= amount;
+        balance[to] += amount;
+    }
+
+    function someCrypticFunctionName(address _addr) public view returns(uint){
+        return balance[_addr];
+    }
 }
