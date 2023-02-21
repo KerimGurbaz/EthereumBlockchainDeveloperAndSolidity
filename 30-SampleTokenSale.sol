028dcede1bd49e5a008e2954416169f44b4712a3
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-21 14:49:22 +0100

    Update 30-SampleTokenSale.sol

--- 30-SampleTokenSale.sol
+++ 30-SampleTokenSale.sol
@@ -1,7 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-Contract TokenSale{
+abstract contract ERC20{
+    function transferFrom(address _from, address _to, uint256 _value) public virtual returns( bool success);
+    function decimals() public virtual view returns(int8);
+}
 
+
+
+contract TokenSale { 
+    uint public tokenPriceInWei = 1 ether;
+
+    ERC20 public token ;
+    address public tokenOwner;
+
+    constructor(address _token){
+
+        tokenOwner = msg.sender;
+        token = ERC20(_token);
+    }
 
 }
